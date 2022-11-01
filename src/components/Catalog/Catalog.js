7b3efc53a3e93d478506cf7124f6867c72c2d951
georgekuchuryan <grishakuchuryan@gmail.com>
2022-11-01 21:45:52 +0200

    checkBox

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,85 +1,50 @@
-import React from "react";
+import React, {useEffect, useState } from "react";
 
 import styles from "./Catalog.module.scss";
 import CatalogItem from "../CatalogItem/CatalogItem";
 import CheckBox from "../checkBox/checkBox";
+import data from "/George_Kuchuryan/ReactApp-v2/src/api/data.json"
+
 
 const Catalog = () => {
-  const productList = [
-    {
-      id: 1,
-      brand: "Apple",
-      name: "Apple Iphone 14 Pro Max",
-      color: "Purple",
-      "memory size": "512 GB",
-      "display diagonal": "6.7",
-      material: "Ceramic Shield",
-      price: "70 000₴",
-      url: "https://content2.rozetka.com.ua/goods/images/big/284924197.jpg",
-    },
-    {
-      id: 2,
-      brand: "Samsung",
-      name: "Samsung Galaxy Fold 4",
-      color: "Graygreen",
-      "memory size": "512 GB",
-      "display diagonal": "6.2",
-      material: "aluminum + glass",
-      price: "79 999₴",
-      url: "https://content1.rozetka.com.ua/goods/images/big/292002198.jpg",
-    },
-    {
-      id: 3,
-      brand: "Xiaomi",
-      name: "Xiaomi Mi 11 Ultra",
-      color: "White",
-      "memory size": "512 GB",
-      "display diagonal": "6.81",
-      material: "Ceramic",
-      price: "44 581₴",
-      url: "https://content.rozetka.com.ua/goods/images/big/288817287.jpg",
-    },
-    {
-      id: 4,
-      brand: "Huawei",
-      name: "Huawei P50 Pocket",
-      color: "White",
-      "memory size": "256 GB",
-      "display diagonal": "6.9",
-      material: "Ceramic + glass",
-      price: "43 420₴",
-      url: "https://content1.rozetka.com.ua/goods/images/big/282509740.jpg",
-    },
-    {
-      id: 5,
-      brand: "Apple",
-      name: "Apple iPhone 13 Pro Max",
-      color: "Alpine Green",
-      "memory size": "256 GB",
-      "display diagonal": "6.7",
-      material: "Ceramic + glass",
-      price: "48 999₴",
-      url: "https://content1.rozetka.com.ua/goods/images/big/259416164.jpg",
-    },
-    {
-      id: 6,
-      brand: "Samsung",
-      name: "Samsung Galaxy S21",
-      color: "Lavender",
-      "memory size": "128 GB",
-      "display diagonal": "6.4",
-      material: "Glass",
-      price: "24 999₴",
-      url: "https://content.rozetka.com.ua/goods/images/big/245951562.jpg",
-    },
-  ];
+
+const [productList, setProductList] = useState(data.productList);
+
+const [brandListFilter, setBrandListFilter] = useState ([]);
+
+useEffect(() => {
+  createBrandFilter();
+}, [])
+
+const createBrandFilter = () => {
+const brands = data.productList.map(product => product.brand);
+setBrandListFilter([...new Set(brands)]);
+
+}
+
+const filterByBrand = (brandName, checked) => {
+	console.log(brandName, checked)
+	console.log(productList)
+    const newProductList = [];
+    if (checked) {
+        productList.forEach(product => {
+			console.log(product)
+            if (brandName === product.brand) {
+                newProductList.push(product);
+            }
+        });
+		console.log('newProductList', newProductList)
+		setProductList(newProductList);
+    }
+}
 
   return (
     <div className={styles.Catalog}>
       <div className="album py-5 bg-light">
         <div className="container">
-                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            {productList.map((item) => {
+         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            {
+            productList.map((item) => {
               return (
                 <div className="col" key={item.id}>
                   <CatalogItem
@@ -92,8 +57,29 @@ const Catalog = () => {
               );
             })}
           </div>
+          <div className=''>
+    <h2 className='filters__title'>Material</h2>
+    <div className='d-flex'>
+        <form>
+            {
+                brandListFilter.map((el, id) => {
+                    return (
+                        <div key={id} className='mb-3'>
+                            <input id={el} type="checkbox"
+                                   checked={el.value}
+                                   onChange={e => filterByBrand(el, e.target.checked)}
+                                  />
+                            <label htmlFor={el}>{el}</label> 
+                        </div>
+                    )
+                })
+            }
+        </form>
+    </div>
+</div>
         </div>
       </div>
+      
     </div>
   );
 };
